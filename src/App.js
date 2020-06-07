d7cee4b713c56526ff5ad44dec5d83f13840825a
Vikrant Bhat <vik.bhat223@gmail.com>
2020-06-07 20:41:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { connect } from 'react-redux';
+
+import {
+	increaseCounter,
+	decreaseCounter,
+} from './redux/Counter/counter.actions';
+
+function App(props) {
+	return (
+		<div className='App'>
+			<div>Count: {props.count}</div>
+			<button onClick={() => props.increaseCounter()}>Increase Count</button>
+			<button onClick={() => props.decreaseCounter()}>Decrease Count</button>
+		</div>
+	);
 }
 
-export default App;
+const mapStateToProps = (state) => {
+	console.log('state:', state);
+	return {
+		count: state.counter.count,
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		increaseCounter: () => dispatch(increaseCounter()),
+		decreaseCounter: () => dispatch(decreaseCounter()),
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// REDUX
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Provider>,
+	document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
